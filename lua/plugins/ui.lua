d2b0cc4b1865ad6f464911885b4d5bfc3def2d2c
wyt551 <3189218276@qq.com>
2024-01-29 12:57:44 +0800

    Add ui and some utils

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,31 @@
+return {
+    {
+        "akinsho/bufferline.nvim",
+        event = "VeryLazy",
+        config = true,
+    },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        event = "VeryLazy",
+        main = "ibl",
+        config = true,
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        event = "VeryLazy",
+        config = true,
+    },
+    {
+        "goolord/alpha-nvim",
+        config = function()
+            require 'alpha'.setup(require 'alpha.themes.dashboard'.config)
+        end
+    },
+    {
+        "RRethy/vim-illuminate",
+        event = "VeryLazy",
+        config = function()
+            require('illuminate').configure()
+        end
+    },
+}
